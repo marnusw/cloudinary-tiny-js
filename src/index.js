39f891e1b570a4eccaaab62680bed556ba8e4f6a
Marnus Weststrate <marnusw@gmail.com>
2017-09-09 15:48:43 +0100

    Default export with test.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import urlBuilder from './urlBuilder'
 import imageParameters from './parameters/image'
 // import videoParameters from './parameters/video'
 
-const image = urlBuilder({
+export default urlBuilder({
   image: imageParameters,
   // video: videoParameters,
 })
-
-export default {
-  image,
-  url: image,
-  // video: urlBuilder({video: videoParameters}, 'video'),
-}
